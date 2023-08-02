09d11e678fe799a9c3fcb1a5419c205ce62725d7
Exekiel Dela Cruz <114486598+exdelacruz@users.noreply.github.com>
2023-08-02 16:51:23 +0900

    Update openai_generate_image_from_text_prompt.py

--- openai_generate_image_from_text_prompt.py
+++ openai_generate_image_from_text_prompt.py
@@ -1,45 +1,51 @@
-# -*- coding: utf-8 -*-
-
-import urllib.request
-from PIL import Image
-import numpy as np
-import matplotlib.pyplot as plt
-
-import openai
-import os
-import matplotlib.pyplot as plt
-import numpy as np
-import urllib.request
-from PIL import Image
-import os
-import urllib.request
-from PIL import Image
-import numpy as np
-import matplotlib.pyplot as plt
+import streamlit as st
 import openai
 
-# Set your OpenAI API Key here. Replace "YOUR_API_KEY" with your actual API key.
-openai.api_key = "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"
+# Set up the OpenAI API
+openai.api_key = st.secrets["api_key"]
 
-def generate_image(input_string): 
-  response = openai.Image.create(
-    prompt=input_string,
-    n=1,
-    size="512x512"
-  )
-  image_url = response['data'][0]['url']
-  return image_url
+
+def generate_code(input_string):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=input_string +"\n",
+        max_tokens=1024,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        temperature=0.0,
+        stop=None,
+    )
+    answer = response.choices[0].text.strip()
+    return answer
+
+def main():
+    st.set_page_config(page_title="C++ Tutorial App")
+    st.title("C++ Tutorial App - Generate Code for Basic Programming Tasks")
+    st.text("by: Ellan V. Flores BSCS 3A")
+    instructions = ['1. Code for a factorial function',
+                    '2. Code to sort an array using bubble sort algorithm',
+                    '3. Code for a linked list data structure',
+                    '4. Code to read and write files in C++']
+
+    str_inst = ''
+    count = 0
+    for i in instructions:
+        str_inst = str_inst + i
+        if count < len(instructions)-1:
+            str_inst = str_inst + ',\n'
+        count += 1
+
+    input_string = "Generate C++ code for the following tasks:\n" + str_inst
+    output = generate_code(input_string)
+
+    print(f"Input: {input_string}\nOutput:\n{output}")
+
+    task = st.selectbox("Select task:", instructions)
+
+    if st.button("Generate code"):
+        output = generate_code(task)
+        st.code(output, language="cpp")
 
 if __name__ == "__main__":
-    input_string = "a bird in the branch of the tree"
-    output = generate_image(input_string)
-    print(f"Input: {input_string}\nOutput: {output}")
-
-    urllib.request.urlretrieve(output, 'output.png')
-    img = Image.open('output.png')
-    img_array = np.array(img)
-
-    plt.figure(figsize=(9, 9))
-    ax = plt.axes(xticks=[], yticks=[])
-    ax.imshow(img_array)
-    plt.show()
+    main()
